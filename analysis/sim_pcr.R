265329f9b824f57ef9c6aba75e8e4bf816322c20
Jimmy O'Donnell <jodonnellbio@gmail.com>
2017-11-29 23:11:51 -0800

    do multiple PCR replicates

--- analysis/sim_pcr.R
+++ analysis/sim_pcr.R
@@ -8,7 +8,16 @@ library(data.table)
 # so add primer efficiency now rather than generate fixed efficiences upfront
 template_dat[, eff := primer_eff(N = templates, mmv = 'low'), by = sample]
 
-pcr_dat <- template_dat[,list(
-  species, 
-  templates = do_pcr(template_copies = templates, template_effs = eff, ncycles = 30)
-  ), by = sample]
+pcr_reps <- 10
+temp <- list()
+for(i in 1:pcr_reps){
+  temp[[i]] <- template_dat[,list(
+    rep.pcr = i, 
+    species, 
+    templates = do_pcr(template_copies = templates, 
+                       template_effs = eff, 
+                       ncycles = 30, inflection = 15, slope = 0.5)
+    ), by = sample]
+}
+pcr_dat <- rbindlist(temp)
+rm(temp)
