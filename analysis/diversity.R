5062a400cb8f7157b5de8f002dd27c678423aa27
Jimmy O'Donnell <jodonnellbio@gmail.com>
2017-12-07 12:06:52 -0800

    add diversity calculations; functionalize plotting

--- analysis/diversity.R
+++ analysis/diversity.R
@@ -4,20 +4,53 @@
 library(vegan)
 
 
-div.in <- samples.env[ , .(simp = diversity(templates, index = "simpson")), by = sample]
-div.in <- template_dat[ , .(simp = diversity(templates, index = "simpson")), by = sample]
+# div.in <- samples.env[ , .(simp = diversity(templates, index = "simpson")), by = sample]
+div.in <- template_dat[ , .(
+  simp.in = diversity(templates, index = "simpson"), 
+  shan.in = diversity(templates, index = "shannon")
+  ), by = sample]
 
 ################################################################################
-div.out <- seq_dat[ , .(simp = diversity(count, index = "simpson")), 
-  by = .(sample.env, pcr.id)]
+div.out <- pcr_dat[ , .(
+  simp.out = diversity(amplicons, index = "simpson"), 
+  shan.out = diversity(amplicons, index = "shannon")
+  ), by = .(sample, rep.pcr)]
 
 div.in
 div.out
 
-
-
-boxplot(simp ~ sample.env, data = div.out, ylim = range(c(div.in[,simp], div.out[,simp])))
-points(div.in[,simp], col = 2, lwd = 2)
+div.full <- merge(div.in, div.out, by = 'sample')
 
 # calculate differences between input and output
-div.out[,simp]
+div.full[, simp.diff := simp.in - simp.out]
+div.full[, shan.diff := shan.in - shan.out]
+div.full <- merge(div.full, unique(template_dat[,.(sample, even, rich)]), by = 'sample')
+
+div.full[ , div.scen := paste(even, rich, sep = '.')]
+
+divplot <- function(mmv, metric){
+  if(metric == "simpson"){
+  	divcol <- quote(simp.diff)
+    XLAB <- "Delta Simpson"
+  }
+  if(metric == "shannon"){
+  	divcol <- quote(shan.diff)
+  	XLAB <- "Delta Shannon"
+  }
+  if(metric == "richness"){
+  	divcol <- quote(rich.diff)
+  	XLAB <- "Delta Richness"
+  }
+  maintext <- paste0('Mismatch Variation = ', mmv)
+  par(mar = c(4, 7, 3, 1))
+  boxplot(div.full[,eval(divcol)] ~ div.full[,div.scen], 
+    # data = div.full, 
+    # ylim = range(c(div.full[,simp.diff], div.full[,simp.diff])), 
+    horizontal = TRUE, las = 1
+  )
+  title(main = maintext, xlab = XLAB)
+  # points(div.full[, simp.in], col = 2, lwd = 2)
+  
+}
+divplot(mmv = 'Low', metric = "simpson")
+
